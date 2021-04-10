fa34f1f8cefc9f512f98d3d355e805c384e641c6
Mikael <mikael.gallucci@escriba.com.br>
2021-04-09 22:58:53 -0300

    LAyouts

--- src/modules/dashboard/Dashboard.vue
+++ src/modules/dashboard/Dashboard.vue
@@ -1,12 +1,74 @@
 <template>
-  <section class="Dashbaord">Dashbaord</section>
+  <div class="Dashbaord">
+    <v-row>
+      <v-col>
+        <h2>Olá, XXXX!</h2>
+      </v-col>
+    </v-row>
+
+    <v-row class="mt-5 pt-5">
+      <v-col
+        v-for="({ actionIcon, actionText, ...attrs }, i) in stats"
+        :key="i"
+        cols="12"
+        md="6"
+        lg="3"
+      >
+        <MaterialStatsCard v-bind="attrs">
+          <template #actions>
+            <v-icon class="mr-2" small v-text="actionIcon" />
+            <div class="text-truncate">
+              {{ actionText }}
+            </div>
+          </template>
+        </MaterialStatsCard>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
+import MaterialStatsCard from "@/components/MaterialStatsCard";
 export default {
   name: "Dashbaord",
+  components: { MaterialStatsCard },
   data() {
-    return {};
+    return {
+      stats: [
+        {
+          actionIcon: "mdi-calendar-range",
+          actionText: "Ultimos 7 dias",
+          color: "success",
+          icon: "mdi-check",
+          title: "Tarefas Concluidas",
+          value: "18"
+        },
+        {
+          actionIcon: "mdi-calendar-range",
+          actionText: "Ultimos 7 dias",
+          color: "primary",
+          icon: "mdi-clock-alert",
+          title: "Tarefas em atraso",
+          value: "3"
+        },
+        {
+          actionIcon: "mdi-calendar-range",
+          actionText: "Ultimas 24 horas",
+          color: "warning",
+          icon: "mdi-calendar-check",
+          title: "Tarefas em aberto",
+          value: "8"
+        },
+        {
+          actionIcon: "mdi-history",
+          actionText: "Ultima atualização há: 5 minutos",
+          color: "info",
+          icon: "mdi-alert-decagram",
+          title: "Novas tarefas",
+          value: "+4"
+        }
+      ]
+    };
   },
   created() {},
   methods: {},
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,45 @@
 <template>
   <section id="inspire">
     <v-navigation-drawer v-model="drawer" app>
-      <!--  -->
+      <template v-slot:prepend>
+        <v-list-item two-line>
+          <v-list-item-avatar>
+            <img src="https://randomuser.me/api/portraits/men/1.jpg" />
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title>Mikael Gallucci</v-list-item-title>
+            <v-list-item-subtitle>Empresa XXX </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </template>
+
+      <v-divider></v-divider>
+
+      <v-list shaped>
+        <v-list-item-group color="primary">
+          <v-list-item v-for="(item, i) in items" :key="i" :to="item.route">
+            <v-list-item-icon>
+              <v-icon v-text="item.icon"></v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.title"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar app>
+    <v-app-bar app height="72" color="secondary" dark>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-toolbar-title color="secondary">EzSight</v-toolbar-title>
     </v-app-bar>
 
     <v-main>
-      <router-view></router-view>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
     </v-main>
   </section>
 </template>
@@ -19,6 +47,15 @@
 <script>
 export default {
   name: "Home",
-  data: () => ({ drawer: null })
+  data: () => ({
+    drawer: null,
+
+    items: [
+      { title: "Dashboard", icon: "mdi-view-dashboard", route: "dashboard" },
+      { title: "Projetos", icon: "mdi-apps-box", route: "projetos" },
+      { title: "Tarefas", icon: "mdi-calendar-check", route: "tarefas" },
+      { title: "Configurações", icon: "mdi-cog", route: "configuracoes" }
+    ]
+  })
 };
 </script>
