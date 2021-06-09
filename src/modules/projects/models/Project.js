64f49f2620eef6ac3811cffd909d1db29898ac40
Mikael <mikael.gallucci@escriba.com.br>
2021-06-09 20:59:15 -0300

    applications

--- src/modules/projects/models/Project.js
+++ src/modules/projects/models/Project.js
@@ -1,5 +1,4 @@
 import Model from "@/models/Model";
-import store from "@/store";
 
 export default class Project extends Model {
   config() {
@@ -7,13 +6,6 @@ export default class Project extends Model {
       resource: "projects",
       storeName: "Project"
     };
-  }
-
-  async beforeSave(record) {
-    delete record.applications;
-    delete record.createdBy;
-    record.tenantId = store.getters["User/getUser"].tenantId;
-    return record;
   }
 }
 
